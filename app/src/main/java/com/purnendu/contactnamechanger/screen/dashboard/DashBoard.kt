a98d808b71240e0a56910217d8471f61be0c196a
PurnenduSamanta <joysamanta123@gmail.com>
2024-12-02 16:04:35 +0530

    16th_commit_02_11_2024

--- app/src/main/java/com/purnendu/contactnamechanger/screen/dashboard/DashBoard.kt
+++ app/src/main/java/com/purnendu/contactnamechanger/screen/dashboard/DashBoard.kt
@@ -1,5 +1,8 @@
 package com.purnendu.contactnamechanger.screen.dashboard
 
+import android.provider.ContactsContract
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,6 +30,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.purnendu.contactnamechanger.components.CustomPhDialog
 import com.purnendu.contactnamechanger.components.TimePicker
@@ -38,6 +42,28 @@ fun DashBoard(
     modifier: Modifier = Modifier,
     viewModel: DashBoardViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
+
+    val context= LocalContext.current
+
+     val contactPickerLauncher = rememberLauncherForActivityResult(ActivityResultContracts.PickContact()) { contactUri ->
+        contactUri?.let {
+            val projection = arrayOf(ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.DISPLAY_NAME, ContactsContract.PhoneLookup.NUMBER)
+            val contentResolver = context.contentResolver
+            // Uri to search the contact using the phone number
+            val cursor = contentResolver.query(it, projection, null, null, null)
+            cursor?.use { cursor ->
+                if (cursor.moveToFirst()) {
+                    val IDIndex = cursor.getColumnIndex(ContactsContract.PhoneLookup._ID)
+                    val nameIndex = cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME)
+                    val numberIndex = cursor.getColumnIndex(ContactsContract.PhoneLookup.NUMBER)
+
+                    val id = if (IDIndex != -1) cursor.getLong(IDIndex) else null
+                    val name = if (nameIndex != -1) cursor.getString(nameIndex) else null
+                    val number = if (numberIndex != -1) cursor.getString(numberIndex) else null
+                }
+            }
+        }
+    }
 
     val scope = rememberCoroutineScope()
 
@@ -57,7 +83,10 @@ fun DashBoard(
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         floatingActionButton = {
-            FloatingActionButton(onClick = { isPhNoDialogVisible.value = true }) {
+            FloatingActionButton(onClick = {
+                contactPickerLauncher.launch(null)
+
+            }) {
 
                 Box(
                     modifier = modifier
